25ef283db6fd15b544c914096b9550ac50b812d9
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2024-05-28 22:28:41 +0530

    Add files via upload

---
+++ UrlShortner/Controllers/UrlShController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using UrlShortner.Models;
+using UrlShortner.Services;
+
+namespace UrlShortner.Controllers
+{
+   
+    public class UrlShController : Controller
+    {
+        
+        // GET: UrlShController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{shortCode}")]
+        public ActionResult redirecturl(string shortCode)
+        {
+            MongoService mongoService = new MongoService();
+            var model = mongoService.FindByShortCode(shortCode);
+            if (model == null)
+            {
+                return NotFound(); // Or handle the case where the shortened URL is not found
+            }
+
+            string redirectUrl = model.OriginalUrl;
+            if(!redirectUrl.StartsWith("http://") && !redirectUrl.StartsWith("https://"))
+            {
+                redirectUrl = "http://" + redirectUrl;
+            }
+
+            mongoService.IncrementVisitorCount();
+            return Redirect(redirectUrl);
+        }
+
+
+
+        [HttpPost]
+        [Route("Urlshort")]
+        public ActionResult GenerateshortUrl([FromQuery] string originalUrl)
+        {
+            MongoService mongoService = new MongoService();
+            try
+            {
+                if (string.IsNullOrEmpty(originalUrl))
+                {
+                    return BadRequest("Original URL cannot be null or empty");
+                }
+
+                string res = UrlService.GenerateShortUrl(originalUrl);
+                mongoService.IncrementVisitorCount();
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/visitorCount")]
+        public IActionResult GetVisitorCount()
+        {
+            MongoService mongoService = new MongoService();
+            var visitorCount = mongoService.GetCountcollection();
+            if (visitorCount == null)
+            {
+                visitorCount = new VisitorCount { Count = 0 };
+                mongoService.insertifnone(visitorCount);
+            }
+            return Ok(visitorCount.Count);
+        }
+
+
+
+    }
+}
---
+++ UrlShortner/Models/shortenerModel.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace UrlShortner.Models
+{
+    public class shortenerModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+        public string CreatedTime { get; set; }
+    }
+
+    public class VisitorCount
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int Count { get; set; }
+       // public string CreatedTime { get; set; }
+    }
+}
