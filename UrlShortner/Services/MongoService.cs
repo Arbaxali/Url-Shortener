761bc4b5aa0ded9fca524cd359fe8f268a350d1b
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2024-05-28 22:30:27 +0530

    Add files via upload

---
+++ UrlShortner/Services/MongoService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UrlShortner.Models;
+namespace UrlShortner.Services
+{
+    public class MongoService
+    {
+        private readonly IMongoCollection<shortenerModel> _service;
+        private readonly IMongoCollection<VisitorCount> _visitorservice;
+
+        public MongoService()
+        {
+            //var connectionString = "mongodb://localhost:27017/";
+            //var databaseName = "urlshortener";
+            //var collectionName = "shortenerCollection";
+
+            var connectionString = "mongodb+srv://arbazaliMongo:PfKKEKtYYNHQYovy@urlshortnercluster.tr3wymk.mongodb.net/";
+            var databaseName = "UrlShortnerDb";
+            var collectionName = "shortenerCollection";
+            var collectionName2 = "visitorsCollection";
+
+
+            //Console.WriteLine(connectionString);
+            //Console.WriteLine(databaseName);
+            //Console.ReadLine();
+            var mongoClientResults = new MongoClient(connectionString);
+            var mongoDatabaseResults = mongoClientResults.GetDatabase(databaseName);
+            _service = mongoDatabaseResults.GetCollection<shortenerModel>(collectionName);
+            _visitorservice = mongoDatabaseResults.GetCollection<VisitorCount>(collectionName2);
+        }
+
+        public  void InsertDatatoMongo(shortenerModel model)
+        {
+            _service.InsertOne(model);
+        }
+
+        public shortenerModel FindByShortCode(string shortCode)
+        {
+            return _service.Find(s => s.ShortenedUrl == shortCode).FirstOrDefault();
+        }
+
+        public void insertifnone(VisitorCount visitor)
+        {
+            _visitorservice.InsertOne(visitor);
+        }
+
+        public VisitorCount GetCountcollection()
+        {
+            return _visitorservice.Find(_ => true).FirstOrDefault();
+        }
+
+        public void IncrementVisitorCount()
+        {
+            var visitorCount = _visitorservice.Find(_ => true).FirstOrDefault();
+            if (visitorCount == null)
+            {
+                visitorCount = new VisitorCount { Count = 1 };
+                _visitorservice.InsertOne(visitorCount);
+            }
+            else
+            {
+                var update = Builders<VisitorCount>.Update.Inc(v => v.Count, 1);
+                _visitorservice.UpdateOne(v => v.Id == visitorCount.Id, update);
+            }
+        }
+    }
+}
---
+++ UrlShortner/Services/UrlService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UrlShortner.Models;
+
+
+namespace UrlShortner.Services
+{
+    public class UrlService
+    {
+
+        public static string GenerateShortUrl(string originalUrl) {
+
+            if (string.IsNullOrWhiteSpace(originalUrl))
+            {
+                throw new ArgumentException("The URL cannot be null or empty.");
+            }
+
+            
+            string pattern = @"^(https?://)?([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}(/.*)?$";
+            if (!Regex.IsMatch(originalUrl, pattern))
+            {
+                throw new ArgumentException("The URL format is invalid.");
+            }
+
+
+            if (Regex.IsMatch(originalUrl, @"^\d+$"))
+            {
+                throw new ArgumentException("The URL cannot contain only numbers.");
+            }
+
+
+
+            MongoService mongoService = new MongoService();
+            string shorturl = GenerateShortCode();
+            
+
+
+            if (!originalUrl.StartsWith("http://") && !originalUrl.StartsWith("https://"))
+            {
+                originalUrl = "http://" + originalUrl;
+            }
+
+
+            shortenerModel model = new shortenerModel
+            {
+                OriginalUrl = originalUrl,
+                ShortenedUrl = shorturl,
+                CreatedTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("India Standard Time")).ToString("yyyy-MM-dd HH:mm:ss")
+,
+            };
+
+            
+            mongoService.InsertDatatoMongo(model);
+            
+            
+            
+            return $"https://localhost:7120/{shorturl}";
+        }
+
+        private static string GenerateShortCode()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            char[] code = new char[6]; // Change the length for shorter/longer codes
+            for (int i = 0; i < code.Length; i++)
+            {
+                code[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(code);
+        }
+    }
+}
